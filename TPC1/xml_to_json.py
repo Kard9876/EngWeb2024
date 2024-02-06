564b8d9dabd9e011c1e9c2af823ab803a4a44015
Kard9876 <dark9876king@gmail.com>
2024-02-06 17:54:09 +0000

    Still working on function that ignores desired tags

--- TPC1/xml_to_json.py
+++ TPC1/xml_to_json.py
@@ -5,8 +5,16 @@
 
 import json, os, xmltodict, xml.etree.ElementTree as ET
 
+print(os.listdir(os.getcwd()))
+
+def add_extra_whitespace(c):
+    if c == ',' or c == '.' or c == '!' or c == '?' or c == ';':
+        return ""
+
+    return " "
+
 # Still not functioning -> it's adding whitespaces and newline char at "random" points
-def ignore_tag(elem, tags, val):
+def ignore_tag(elem, tags):
     for c in list(elem):
         if val:
             print(c)
@@ -14,53 +22,27 @@ def ignore_tag(elem, tags, val):
         if c.tag in tags:
             idx = list(elem).index(c)
             if c.text:
-                if val:
-                    print("C text")
-                    print(c.text)
                 if elem.text:
-                    elem.text += c.text
-                    if val:
-                        print("Added C text Elem text")
-                        print(elem.text)
+                    elem.text += ' ' + c.text.strip()
                 else:
-                    elem.text = c.text
-                    if val:
-                        print("Created C text Elem text")
-                        print(elem.text)
+                    elem.text = ' ' + c.text.strip()
             if c.tail:
-                if val:
-                    print("C tail")
-                    print(c.tail)
                 if idx + 1 < len(elem):
                     if elem[idx + 1].text:
-                        elem[idx + 1].text = c.tail + elem[idx + 1].text
-                        if val:
-                            print("Not last element Added C tail Elem text")
-                            print(elem.text)
+                        elem[idx + 1].text = ' ' + c.tail.strip() + elem[idx + 1].text
                     else:
-                        elem[idx + 1].text = c.tail
-                        if val:
-                            print("Not last element Created C tail Elem text")
-                            print(elem.text)
+                        elem[idx + 1].text = ' ' + c.tail.strip()
                 else:
                     if elem.tail:
-                        elem.tail += c.tail
-                        if val:
-                            print("Last elem Added C tail Elem tail")
-                            print(elem.tail)
+                        elem.tail += ' ' + c.tail.strip()
                     else:
-                        elem.tail = c.tail
-                        if val:
-                            print("Last elem Created C tail Elem tail")
-                            print(elem.tail)
-                    elem.text += elem.tail.strip()
+                        elem.tail = ' ' + c.tail.strip()
+
+                    elem.text += ' ' + elem.tail.strip()
 
             elem.remove(c)
         else:
-            ignore_tag(c, tags, val)
-
-        if val:
-            print()
+            ignore_tag(c, tags)
 
 # file_dir = "/mnt/c/Users/Guilherme\ Barbosa/Desktop/UM/3Ano2Semestre/EngWeb/Repo/EngWeb2024/TPC1/MapaRuas-materialBase/MapaRuas-materialBase/texto"
 file_dir = "./MapaRuas-materialBase/texto"
@@ -73,7 +55,7 @@ for filename in os.listdir(file_dir):
     f = open(file_dir + f"/{filename}")
 
     xml_str = ET.fromstring(f.read())
-    ignore_tag(xml_str, ["lugar", "data", "entidade"], val)
+    ignore_tag(xml_str, ["lugar", "data", "entidade"])
     new_xml_str = ET.tostring(xml_str, encoding='unicode')
 
     file_json = xmltodict.parse(new_xml_str)

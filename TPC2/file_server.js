00f7c0bf4c2cb8b9c487a5163d3da069f3ddc0fd
Kard9876 <dark9876king@gmail.com>
2024-02-20 16:35:49 +0000

    File server

--- TPC2/file_server.js
+++ TPC2/file_server.js
@@ -5,29 +5,83 @@ const fs = require('fs')
 const config = require('./config.json')
 const port = config.PORT
 
-const file_dir = './MapaVirtualSite/'
+const file_dir = 'MapaVirtualSite/'
+
+function read_file(filename, res){
+    var ans = {
+        "err" : true,
+        "msg" : ""
+    }
+    
+    fs.readFile(filename, function (err, data){
+        let msg = ""
+        let err_val = false
+
+        if(err){
+            err_val = true
+            msg = err
+            bad_request(res)
+            console.log(err)
+        } else {
+            let html_regex = /.*\.html/
+            if (html_regex.test(filename)) good_request_html(res, data)
+            else good_request_css(res, data)
+        }
+
+        ans = {
+            "err" : err_val,
+            "msg" : msg
+        }
+    })
+}
+
+function good_request_html(res, data){
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.write(data)
+    res.end()
+}
+
+function good_request_css(res, data){
+    res.writeHead(200, { 'Content-Type': 'text/css; charset=utf-8' })
+    res.write(data)
+    res.end()
+}
+
+function bad_request(res){
+    res.writeHead(400, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.write('<h1>There as been an error loading the required service.</h1>')
+    res.write('<h2>We are sorry for the inconvenience.</h2>')
+    res.end()
+}
 
 http.createServer(function (req, res) {
     let query = url.parse(req.url, true)
 
+    
     let path = query.pathname
+    
+    let index_page = /^\/$/
 
-    // TODO verify type of request, verify if file exists, index.html only need empty query, etc
-    filename = file_dir + path + '.html'
+    let city_page = /^\/c[0-9]+$/
+    
+    let style_sheet = /^\/.*\.css$/
+    
+    let filename = ""
+    
+    if (index_page.test(path)){
+        filename = file_dir + 'index.html'
+        
+        read_file(filename, res)
 
-    // FIX ME
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            console.log(err)
-            res.writeHead(400, { 'Content-Type': 'txt/html; charset=utf-8' })
-            res.write('<h1>There as been an error loading the required service.</h1>')
-            res.write('<h2>We are sorry for the inconvenience.</h2>')
-            res.end()
-        } else {
-            res.writeHead(200, { 'Content-Type': 'txt/html; charset=utf-8' })
-            res.write(data)
-            console.log(data)
-            res.end()
-        }
-    })
+    } else if (city_page.test(path)) {
+        filename = file_dir + path.substring(1) + '.html'
+
+        read_file(filename, res)
+        
+    } else if (style_sheet.test(path)) {
+        filename = file_dir + path.substring(1)
+        
+        read_file(filename, res)
+        
+    } else bad_request(res)    
 }).listen(port)
